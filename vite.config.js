9f57e1aa1d2ed799f2f17e745ae8d84525281e05
Ed Preston <epreston@prestonsoft.com>
2023-08-12 04:40:39 +1000

    Update vite.config.js
    cleanup

--- vite.config.js
+++ vite.config.js
@@ -5,24 +5,22 @@
 
 import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   // assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.m4a'], // additional asset types
   // define: {
   //   // ensure inline tests are removed in production builds
   //   'import.meta.vitest': 'undefined',
   // },
-  // server: {
-  //   open: '/index.html',
-  // },
   // plugins: [vue(), splitVendorChunkPlugin()],
   build: {
-    target: ['es2022', 'edge112', 'firefox112', 'chrome112', 'safari16.4', 'ios16.4'],
+    target: ['es2022', 'edge112', 'firefox112', 'chrome112', 'safari16.4', 'ios16.4']
   },
   test: {
     // globals: true,
     // disable threads on GH actions to speed it up
     threads: !process.env.GITHUB_ACTIONS,
-    setupFiles: ['./scripts/vitest-setup.js'],
+    setupFiles: ['./scripts/vitest-setup.js']
     // includeSource: ['src/**/*.{js,ts}'], // enable inline tests
-  },
+  }
 });
