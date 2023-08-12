6ca1f12576476c90d6bbc6f6240fab7e0a3af394
Ed Preston <epreston@prestonsoft.com>
2023-08-13 05:20:45 +1000

    Update global.d.ts
    minor formatting

--- global.d.ts
+++ global.d.ts
@@ -5,12 +5,11 @@
 // for tests
 declare namespace jest {
   interface Matchers<R, T> {
-    toHaveBeenWarned(): R
-    toHaveBeenWarnedLast(): R
-    toHaveBeenWarnedTimes(n: number): R
+    toHaveBeenWarned(): R;
+    toHaveBeenWarnedLast(): R;
+    toHaveBeenWarnedTimes(n: number): R;
   }
 }
-
 
 // additional import types not included in vite/client
 // declare module '*.m4a' {
